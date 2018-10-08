42bc13b1739f794eae638ba51f493c3cde674d5a
Thomas Heavey <thomasjheavey@gmail.com>
2018-10-08 19:20:04 -0400

    more reasonable scaling to energy distribution

--- remd_model/walker.py
+++ remd_model/walker.py
@@ -31,7 +31,7 @@ class Walker(object):
 
     @property
     def energy(self) -> float:
-        return normal(self.temp, self.temp/50.)
+        return normal(self.temp, self.temp/10.)
 
     @property
     def index(self) -> int:
