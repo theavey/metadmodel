b244d4f8dbe698d56cedf9733a7d8affa319ed44
Thomas Heavey <thomasjheavey@gmail.com>
2018-10-09 10:42:34 -0400

    Update year in module docstrings

--- remd_model/replicas.py
+++ remd_model/replicas.py
@@ -1,7 +1,7 @@
 """
 This defines the replicas class.
 
-Copyright (C) 2017 Thomas John Heavey IV
+Copyright (C) 2018 Thomas John Heavey IV
 
 This program is free software: you can redistribute it and/or modify it under the terms of the
 GNU General Public License as published by the Free Software Foundation, either version 3 of
